1dae2384bb69031e56104a49e8a50f5bff4f3bd1
niksm96 <mundargi95@gmail.com>
2019-01-24 19:17:48 +0530

    24/01/2018

--- ServletJDBCApp/src/com/bridgelabz/filter/Validation.java
+++ ServletJDBCApp/src/com/bridgelabz/filter/Validation.java
@@ -14,28 +14,36 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class Validation implements Filter {
-	static final String REGEX_EMAIL_ID = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\\\.[A-Z]{2,6}$";
-	static final String REGEX_PASSWORD = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
-	static final String REGEX_CONTACT = "x{10}";
+	 static final String REGEX_EMAIL_ID = "^[a-z0-9._%-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+	 static final String REGEX_PASSWORD = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
+	 static final String REGEX_CONTACT = "^[0-9]{10}$";
 	
 	@Override
 	public void init(FilterConfig arg0) throws ServletException {}
 
-	
-
 	@Override
-	public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain arg2)
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chainObj)
 			throws IOException, ServletException {
 		RequestDispatcher requestDispatcher=null;
-		HttpServletResponse response=(HttpServletResponse)arg1;
+		HttpServletResponse response=(HttpServletResponse)res;
 		PrintWriter printWriter = response.getWriter();
 		printWriter.println("<html><body>");
-		HttpServletRequest request=(HttpServletRequest) arg0;
-		String emailid=request.getParameter("username");
+		HttpServletRequest request=(HttpServletRequest) req;
+		String emailid=request.getParameter("emailid");
 		String password=request.getParameter("password");
-		
-		
+		String contact=request.getParameter("contact");
+		if(emailid.matches(REGEX_EMAIL_ID) && (password.matches(REGEX_PASSWORD))&& (contact.matches(REGEX_CONTACT))) {
+			chainObj.doFilter(request, response);
+		}
+		else {
+			printWriter.println("Username or password or contact is invalid!");
+			requestDispatcher=request.getRequestDispatcher("/Index2.html");
+			requestDispatcher.include(request, response);
+		}
+		printWriter.println("</html></body>");
+		printWriter.close();
 	}
+	
 	@Override
 	public void destroy() {}
 	
